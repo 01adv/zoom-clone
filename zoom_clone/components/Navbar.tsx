9f70b77aed91e017d5aa8b4422b02070697ec2fc
Advaita <148793147+01adv@users.noreply.github.com>
2024-04-08 14:07:07 +0530

    clerk auth done

--- zoom_clone/components/Navbar.tsx
+++ zoom_clone/components/Navbar.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import MobileNav from "./MobileNav";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
@@ -20,6 +21,12 @@ const Navbar = () => {
       </Link>
       <div className="flex flex-between gap-5">
         {/* clerk user management */}
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        {/* <SignedOut>
+          <SignInButton/>
+        </SignedOut> */}
         <MobileNav />
       </div>
     </nav>
